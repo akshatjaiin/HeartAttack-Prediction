a0fc626c6c165af63f3e2cd2a9c0621fe457cea7
Yash Jain <akshat0@yahoo.com>
2024-08-02 12:10:15 +0530

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,5 +1,6 @@
 # Code you have previously used to load data
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 # Path of the file to read
 case_file_path = 'data.xlsx'
@@ -10,3 +11,24 @@ print(attack_data.columns)
 
 y = attack_data.target
 feature_names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+X = attack_data[feature_names]
+print(X.describe)
+
+#For model reproducibility, set a numeric value for random_state when specifying the model
+attack_model = DecisionTreeRegressor(random_state = 1)
+
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+# Define model
+attack_model = DecisionTreeRegressor()
+# Fit model
+attack_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = attack_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
